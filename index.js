56fc5f16cce9bfeb25c0facafc358dcbdade5e0a
LB (Ben) Johnston <github@lb.ee>
2014-10-03 09:20:24 +1000

    Further testing/mucking around

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ var bundleStatic = function(appDir, buildDir) {
   //console.log(buildDir, fs.realpathSync(buildDir));
   buildDir = fs.realpathSync(buildDir);
   appDir = fs.realpathSync(appDir);
+  //This is the folder /bundle that the app gets put
+  var bundleDir = path.join(buildDir, 'bundle');
   //console.log(buildDir, appDir);
   // create a new folder in sub-folder 'build'
   //later.
@@ -26,38 +28,41 @@ var bundleStatic = function(appDir, buildDir) {
   // }
 
   // create a tmp folder
-  var tempDir = path.join(buildDir, 'tmp');
-  //console.log('buildDir', buildDir, 'tempDir', tempDir);
-  if (fs.existsSync(tempDir)) {
-    console.log('temp directory:', tempDir, 'exists');
-  } else {
-    fs.mkdirSync(tempDir);
-  }
+  // var tempDir = path.join(buildDir, 'tmp');
+  // //console.log('buildDir', buildDir, 'tempDir', tempDir);
+  // if (fs.existsSync(tempDir)) {
+  //   console.log('temp directory:', tempDir, 'exists');
+  // } else {
+  //   fs.mkdirSync(tempDir);
+  // }
 
   // get the bundle into a directory
-  var bundleFile = path.join(tempDir, 'bundle.tar.gz');
-  bundle(appDir, bundleFile);
+  //var bundleDir = path.join(tempDir, 'bundle.tar.gz');
+  bundle(appDir, buildDir);
+
   //console.log('current dir:', process.cwd());
+
+  // grab the HTML, JS, CSS
+  var clientApp = path.join(bundleDir, 'programs', 'web.browser');
+
 };
 
-var bundle = function(appDir, bundleFile) {
-  var buildCommand = '';
+var bundle = function(appDir, bundleDir) {
+  var bundleCommand = 'meteor build ' + bundleDir + ' --debug --directory';
   //console.log(appDir, bundleFile);
 
   //console.log('Starting directory: ' + process.cwd());
   try {
     process.chdir(appDir);
-    //console.log('New directory: ' + process.cwd());
-  }
-  catch (err) {
+    console.log('Change directory: ' + process.cwd());
+  } catch (err) {
     console.log('chdir: ' + err);
   }
 
-
-
-  exec('ls -l', function(error, stdout, stderr) {
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
+  console.log('About to build bundle');
+  exec(bundleCommand, function(error, stdout, stderr) {
+    //console.log('stdout: ' + stdout);
+    //console.log('stderr: ' + stderr);
     if (error !== null) {
       console.log('exec error: ' + error);
     }
